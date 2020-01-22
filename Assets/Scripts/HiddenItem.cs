f0d49e93ea1947d608d2d4a7b2cf7353baa4d3d4
Sondre Agledahl <sondre.milch@gmail.com>
2020-01-22 18:43:11 +0000

    Start of item-finding scripts added

---
+++ Assets/Scripts/HiddenItem.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HiddenItem : MonoBehaviour
+{
+    public delegate void ItemEvent(Type type);
+    public static event ItemEvent OnItemFound;
+
+    public enum Type { BROWN_CHEESE, HEADLESS_FISH, BROKEN_BOTTLE, PINEAPPLE_PIZZA, MARGARINE }
+
+    [SerializeField]
+    private Type _type;
+
+    private Collider _collider;
+
+    private void Awake()
+    {
+        _collider = GetComponent<Collider>();
+    }
+
+    public void Reveal()
+    {
+        // TODO
+        _collider.enabled = false;
+        OnItemFound(_type);
+
+    }
+
+
+}
---
+++ Assets/Scripts/ListItem.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ListItem : MonoBehaviour
+{
+    [SerializeField]
+    private HiddenItem.Type _type;
+
+    public bool Found { get; private set; }
+
+    private Image _icon;
+
+    private Image _checkmark;
+
+    private void OnEnable()
+    {
+        HiddenItem.OnItemFound += OnItemFound;
+    }
+
+    private void Awake()
+    {
+            
+    }
+
+    private void OnItemFound(HiddenItem.Type type)
+    {
+        if (type == _type && !Found)
+        {
+            // TODO
+            Found = true;
+        }
+    }
+
+
+    private void OnDisable()
+    {
+        HiddenItem.OnItemFound -= OnItemFound;
+    }
+
+
+}
