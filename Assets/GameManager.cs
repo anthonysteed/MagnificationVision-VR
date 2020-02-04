d188eda576aa6f4d77b434dd39e5e8dfc23b7365
6.12A <nobody@nobody.ucl.ac.uk>
2020-02-04 16:18:48 +0000

    tutorial section completed

---
+++ Assets/GameManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance { get; private set; }
+
+    [SerializeField]
+    private Animator[] _doorAnimators;
+
+    private void Awake()
+    {
+        if (Instance != null)
+        {
+            Debug.LogError("More than one GameManager!!!!");
+        }
+        Instance = this;
+
+        TutorialSign.OnTutorialComplete += OnTutorialComplete;
+        Debug.Log("GameManager awake called");
+    }
+
+    private void OnTutorialComplete()
+    {
+        Debug.Log("GameManager registered tutorial complete");
+        foreach (Animator animator in _doorAnimators)
+        {
+            animator.SetTrigger("openTrigger");
+        }
+
+        TutorialSign.OnTutorialComplete -= OnTutorialComplete;
+    }
+
+
+}
---
+++ Assets/TutorialSign.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialSign : MonoBehaviour
+{
+    public delegate void TutorialEvent();
+    public static event TutorialEvent OnTutorialComplete;
+
+    private static readonly int NUMBER_OF_SIGNS = 5;
+
+    private static int s_NextSign = 1;
+
+    [SerializeField]
+    [Range(1, 5)]
+    private int _number;
+
+    private Transform _player;
+
+    private LerpAlpha _fadeEffect;
+
+    private void Awake()
+    {
+        _player = Camera.main.transform;
+        _fadeEffect = GetComponent<LerpAlpha>();
+    }
+
+    private void Update()
+    {
+        if (s_NextSign == _number)
+        {
+            float distToPlayer = Vector3.Distance(_player.position, transform.position);
+            if (distToPlayer < 2f)
+            {
+                Complete();
+            }
+        }
+    }
+
+    private void Complete()
+    {
+        if (s_NextSign == NUMBER_OF_SIGNS)
+        {
+            OnTutorialComplete();
+        }
+        s_NextSign++;
+        _fadeEffect.FadeWithEmission();
+        enabled = false;
+        Destroy(gameObject, 2f);
+    }
+
+
+}
